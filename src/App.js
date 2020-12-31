09c62d7634f45198912cd1148d32b246617dead2
lapimpact2020 <wildhyeonjin@gmail.com>
2020-12-31 14:46:50 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'http://placeimg.com/64/64/1',
+  'name': 'oh',
+  'birthday': '820521',
+  'gender': 'mail',
+  'job': 'student'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': 'nao',
+    'birthday': '860918',
+    'gender': 'femail',
+    'job': 'nurse'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': 'lee',
+    'birthday': '860101',
+    'gender': 'mail',
+    'job': 'student'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello react!
-        </p>
-      </header>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
